7feb2a639c51088ad250a33a2d40dbd5eb8e0978
Apurba <apurbabarui26@gmail.com>
2024-10-21 01:59:16 +0530

    update

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\DashbordController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('admin')->group(function(){
+    Route::get('dashboard', [DashbordController::class, 'index'])->name('admin.dashboard'); 
 });
